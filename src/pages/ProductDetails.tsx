f4bb8305dfe2ac68967e6090116368877abe6528
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-12-08 22:03:32 +0600

    first commit

---
+++ src/pages/ProductDetails.tsx
@@ -0,0 +1,30 @@
+import ProductReview from "@/components/ProductReview";
+import { Button } from "@/components/ui/button";
+import { useSingleProductQuery } from "@/redux/features/products/productApi";
+import { useParams } from "react-router-dom";
+
+export default function ProductDetails() {
+  const { id } = useParams();
+  const { data: productData } = useSingleProductQuery(id);
+
+  return (
+    <>
+      <div className="flex max-w-7xl mx-auto items-center border-b border-gray-300">
+        <div className="w-[50%]">
+          <img src={productData?.image} alt="" />
+        </div>
+        <div className="w-[50%] space-y-3">
+          <h1 className="text-3xl font-semibold">{productData?.name}</h1>
+          <p className="text-xl">Rating: {productData?.rating}</p>
+          <ul className="space-y-1 text-lg">
+            {productData?.features?.map((feature: string) => (
+              <li key={feature}>{feature}</li>
+            ))}
+          </ul>
+          <Button>Add to cart</Button>
+        </div>
+      </div>
+      <ProductReview id={id!} />
+    </>
+  );
+}
